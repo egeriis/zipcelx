9f73ff36142cf41d72b1a02591faa2f92cb2553c
Lars Krieger <lars.krieger@gmail.com>
2017-02-22 18:20:42 +0100

    Fixed linting error in constants js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-const CELL_TYPE_STRING = 'string';
-const CELL_TYPE_NUMBER = 'number';
-const validTypes = [CELL_TYPE_STRING, CELL_TYPE_NUMBER];
+export const CELL_TYPE_STRING = 'string';
+export const CELL_TYPE_NUMBER = 'number';
+export const validTypes = [CELL_TYPE_STRING, CELL_TYPE_NUMBER];
