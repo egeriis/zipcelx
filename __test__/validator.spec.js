6303e61774cb95b7eb54857a1c48b6b0340bb080
Lars Krieger <lars.krieger@gmail.com>
2017-02-23 17:28:59 +0100

    Updated validator.spec to pass new validator config arg

--- __test__/validator.spec.js
+++ __test__/validator.spec.js
@@ -1,20 +1,25 @@
 import validator from '../src/validator';
 
-const data = [
-  [{
-    value: 'Monkey',
-    type: 'string'
-  }, {
-    value: 1000,
-    type: 'number'
-  }]
-];
+const config = {
+  filename: 'report',
+  sheet: {
+    data: [
+      [{
+        value: 'Monkey',
+        type: 'string'
+      }, {
+        value: 1000,
+        type: 'number'
+      }]
+    ]
+  }
+};
 
 describe('Sheet Data validator', () => {
   it('Should ensure that Data passed is an Array', () => {
-    expect(validator(data)).toBe(true);
+    expect(validator(config)).toBe(true);
   });
   it('Should ensure that argument Data\'s childs is arrays', () => {
-    expect(validator(data)).toBe(true);
+    expect(validator(config)).toBe(true);
   });
 });
