0da3df030be052b6083fb0a997850204b7837d61
Ronald Wind <ronald@digitpaint.nl>
2018-03-20 12:10:58 +0100

    Add support for more than 26 columns

--- __test__/commons/generatorCellNumber.spec.js
+++ __test__/commons/generatorCellNumber.spec.js
@@ -4,4 +4,16 @@ describe('Cell number generator', () => {
   it('should create cell number A1', () => {
     expect(generatorCellNumber(0, 1)).toBe('A1');
   });
+
+  it('should create cell number AD3', () => {
+    expect(generatorCellNumber(29, 3)).toBe('AD3');
+  });
+
+  it('should create cell number BAR7', () => {
+    expect(generatorCellNumber(1395, 7)).toBe('BAR7');
+  });
+
+  it('should create cell number FOOBAR44', () => {
+    expect(generatorCellNumber(78407931, 44)).toBe('FOOBAR44');
+  });
 });
