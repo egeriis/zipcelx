80034aae01fb97fac29b3236fe936b028afa5883
Esmee Peters <esmee@digitpaint.nl>
2018-04-03 14:24:07 +0200

    Fix typos in constants

--- src/commons/constants.js
+++ src/commons/constants.js
@@ -2,9 +2,9 @@ export const CELL_TYPE_STRING = 'string';
 export const CELL_TYPE_NUMBER = 'number';
 export const validTypes = [CELL_TYPE_STRING, CELL_TYPE_NUMBER];
 
-export const MISSING_KEY_FILENAME = 'Zipclex config missing property filename';
+export const MISSING_KEY_FILENAME = 'Zipcelx config missing property filename';
 export const MISSING_KEY_TITLE = 'Zipcelx config missing property sheet title';
-export const INVALID_TYPE_FILENAME = 'Zipclex filename can only be of type string';
+export const INVALID_TYPE_FILENAME = 'Zipcelx filename can only be of type string';
 export const INVALID_TYPE_TITLE = 'Zipcelx sheet title is not of type string';
 export const INVALID_TYPE_SHEET = 'Zipcelx sheet data is not of type array';
 
